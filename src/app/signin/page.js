ec83e27c5f317c84c999b87f63a0292592cd9523
rahulram21 <rahulkannom21@gmail.com>
2025-01-06 12:51:33 +0530

    mobile responsiveness works fine

--- src/app/signin/page.js
+++ src/app/signin/page.js
@@ -1,10 +1,10 @@
-import SignIn from "@/components/signIn"
-import SignUpCardHolder from "@/components/custom-ui/signUpCardHolder"
+import SignIn from "@/components/signIn";
+import SignUpCardHolder from "@/components/custom-ui/signUpCardHolder";
 
 export default function SignInPage(){
 
     return(
-        <div className="flex justify-center items-center min-h-screen bg-signup-bg relative">
+        <div className="flex justify-center items-center min-h-screen w-full bg-signup-bg relative">
             <SignUpCardHolder>
                 <SignIn />
             </SignUpCardHolder>
--- src/components/custom-ui/signUpCardHolder.js
+++ src/components/custom-ui/signUpCardHolder.js
@@ -5,10 +5,24 @@ const signUp_bgImg_gradient = "/assets/signup-bgImg-gradient.png";
 export default function SignUpCardHolder({ children }) {
   return (
     <div className="flex items-center justify-center w-full">
-      <img src={signUp_bgImg_left} alt="bg-IMG" />
-      <img src={signUp_bgImg_gradient} alt="grad-bg" className="absolute" />
-      <div className="relative z-20 w-1/4">{children}</div>
-      <img src={signUp_bgImg_right} alt="bg-I9MG" />
+      <img
+        src={signUp_bgImg_left}
+        alt="bg-IMG"
+        className="hidden lg:block w-auto max-w-full"
+      />
+      <img
+        src={signUp_bgImg_gradient}
+        alt="grad-bg"
+        className="absolute w-full h-full object-cover"
+      />
+      <div className="relative z-20 w-full md:w-2/4 lg:w-1/4 px-4 md:px-0">
+        {children}
+      </div>
+      <img
+        src={signUp_bgImg_right}
+        alt="bg-I9MG"
+        className="hidden lg:block w-auto max-w-full"
+      />
     </div>
   );
 }
